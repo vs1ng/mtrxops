42dea3499eca9924b024ab83889aee4563b62559
vinayak <imnot@givingmyemail>
2026-06-07 19:42:58 +0530

    DOING: threading testing

--- test.c
+++ test.c
@@ -5,13 +5,21 @@
 
 pthread_mutex_t lock;
 
+atomic_int C = ATOMIC_VAR_INIT(0); 
+
 void* F1(void* arg){
-    puts("F1");
+    for(int i = 0; i < 4; i++){
+        printf("F1: i = %i\t C = %i\n",i,C);
+        atomic_fetch_add(&C,i);
+    }
     return NULL;
 }
 
 void* F2(void* arg){
-    puts("F2");
+    for(int j = 0; j < 4; j++){
+        printf("F2: j = %i\tC = %i\n",j,C);
+        atomic_fetch_add(&C,j);
+    }
     return NULL;
 }
 
