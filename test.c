edbd94aed45a4065d607431f1d8e6a13db0a1fd9
vinayak <imnot@givingmyemail>
2026-06-07 15:09:42 +0530

    DOING: threading

--- test.c
+++ test.c
@@ -1,17 +1,33 @@
-#include <pthread.h>
 #include <stdio.h>
+#include <stdatomic.h>
+#include <pthread.h>
+#define r0 return 0
 
-void* run(void* arg){
-    puts("hi from thread!");
-    pthread_exit(NULL);
-    puts("bye from thread!");
+pthread_mutex_t lock;
+
+void* F1(void* arg){
+    puts("F1");
+    return NULL;
+}
+
+void* F2(void* arg){
+    puts("F2");
     return NULL;
 }
 
 int main(void){
-    pthread_t T;
-    pthread_create(&T,NULL,run,NULL);
-    pthread_join(T,NULL);
-    puts("bye from program!");
-    return 0;
+    pthread_mutex_init(&lock,NULL);
+    pthread_t T1;
+    pthread_t T2;
+
+    pthread_create(&T1,NULL,F1,NULL);
+    pthread_create(&T2,NULL,F2,NULL);
+
+    pthread_join(T1,NULL);
+    pthread_join(T2,NULL);
+
+    pthread_mutex_destroy(&lock);
+    r0;
 }
+
+
