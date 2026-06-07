b3a0d8b8ece2940ac357eabaeacb0534d9368773
vinayak <imnot@givingmyemail>
2026-06-07 21:59:49 +0530

    DOING: scalar addition

---
+++ mscalarmul.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#define r0 return 0
+#pragma optimize("O3")
+
+struct row{
+    int r_a;
+    int r_b;
+};
+
+struct col{
+    int c_a;
+    int c_b;
+};
+
+struct sizes{
+    struct row R;
+    struct col C;
+};
+
+void catch(int sig){
+    puts("Memory access illegally attempted!");
+    exit(EXIT_FAILURE);
+}
+
+void bye(int* ptr){
+    free(ptr);
+    ptr = NULL;
+}
+
+void display(int* M, int rc, int cc){
+    for(int i = 0; i < rc; i++){
+        printf(" | ");
+        for(int j = 0; j < cc; j++){
+            printf(" %i ",*(M+i+j));
+        }
+        printf(" |\n");
+    }
+}
+
+void ScalarMul(int K, int* A, int RA, int CA){
+    for(int i = 0; i < RA; i++){
+        for(int j = 0; j < CA; j++){
+            *(A+i+j) = K*(int)(*(A+i+j));
+        }
+    }
+    printf(" %i(A) = \n",K);
+    display(A+0+0,RA,CA);
+}
+
+int main(void){
+    signal(SIGSEGV,catch);
+    struct sizes DATA;
+
+    int A[2][2] = {
+                    {1,2},
+                    {3,4}
+    };
+
+    int B[2][2] = {
+                    {4,5},
+                    {6,7}
+    };
+
+    DATA.R.r_a = (int)(sizeof(A)/sizeof(A[0]));
+    DATA.C.c_a = (int)(sizeof(A[0])/sizeof(A[0][0]));
+    
+    DATA.R.r_b = (int)(sizeof(B)/sizeof(B[0]));
+    DATA.C.c_b = (int)(sizeof(B[0])/sizeof(B[0][0]));
+
+    puts("A:");
+    display(&A[0][0],DATA.R.r_a,DATA.C.c_a);
+
+    puts("B:");
+    display(&B[0][0],DATA.R.r_b,DATA.C.c_b);
+    r0;
+}
