7dc907bad780ce417a3adc4a7739efa068f9ba62
vinayak <imnot@givingmyemail>
2026-06-10 22:46:38 +0530

    Working on threading

--- test.c
+++ test.c
@@ -1,41 +1,28 @@
 #include <stdio.h>
-#include <stdatomic.h>
 #include <pthread.h>
-#define r0 return 0
 
-pthread_mutex_t lock;
+pthread_mutex_t LOCK;
 
-atomic_int C = ATOMIC_VAR_INIT(0); 
+void* F1(void* A){
 
-void* F1(void* arg){
-    for(int i = 0; i < 4; i++){
-        printf("F1: i = %i\t C = %i\n",i,C);
-        atomic_fetch_add(&C,i);
-    }
     return NULL;
 }
 
-void* F2(void* arg){
-    for(int j = 0; j < 4; j++){
-        printf("F2: j = %i\tC = %i\n",j,C);
-        atomic_fetch_add(&C,j);
-    }
+void* F2(void* A){
+
     return NULL;
 }
 
 int main(void){
-    pthread_mutex_init(&lock,NULL);
+    pthread_mutex_init(&LOCK,NULL);
+
     pthread_t T1;
     pthread_t T2;
 
     pthread_create(&T1,NULL,F1,NULL);
     pthread_create(&T2,NULL,F2,NULL);
 
-    pthread_join(T1,NULL);
-    pthread_join(T2,NULL);
-
-    pthread_mutex_destroy(&lock);
-    r0;
+    pthread_mutex_destroy(&LOCK);
+    
+    return 0;
 }
-
-
